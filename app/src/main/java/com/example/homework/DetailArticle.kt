b430ec7d1c4489a947c715da9f2b3fcbf4535ba5
stephenwang0317 <stephenwang0317@gmail.com>
2023-06-05 22:30:44 +0800

    test the use of coil

--- app/src/main/java/com/example/homework/DetailArticle.kt
+++ app/src/main/java/com/example/homework/DetailArticle.kt
@@ -9,10 +9,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Schedule
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -20,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -60,10 +58,39 @@ fun DetailArticle(
             )
         }
     ) {
-
-        Column {
-            DetailContent(viewModel = viewModel)
-
+        if (viewModel.loading) {
+            Column(
+                modifier = modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(100.dp),
+                    color = Purple500
+                )
+            }
+        } else {
+            Column(
+                modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+            ) {
+                DetailContent(viewModel = viewModel)
+                Row(
+                    modifier = Modifier.padding(
+                        start = 5.dp,
+                        end = 5.dp,
+                        top = 10.dp,
+                        bottom = 10.dp
+                    ),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Divider(thickness = 5.dp, modifier = Modifier.weight(1f))
+                    Text(text = "The End", fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                    Divider(thickness = 5.dp, modifier = Modifier.weight(1f))
+                }
+            }
         }
     }
 }
@@ -79,111 +106,108 @@ fun DetailContent(
     val coroutineScope = rememberCoroutineScope()
 
     Log.i("***********", viewModel.artItem.toString())
-    if (viewModel.loading) {
-        Column(
-            modifier = modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            CircularProgressIndicator(
-                modifier = Modifier.size(100.dp),
-                color = Purple500
+
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Text(
+            text = viewModel.artItem.artTitle ?: "",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(
+                start = 5.dp, end = 5.dp,
+                top = 10.dp, bottom = 10.dp
             )
+        )
+
+        Row(
+            modifier = Modifier
+                .padding(end = 5.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Icon(Icons.Default.Person, contentDescription = null)
+            Text(text = viewModel.artItem.artAuthorName ?: "")
+            Spacer(modifier = Modifier.width(10.dp))
+            Icon(Icons.Default.Schedule, null)
+            Text(text = viewModel.artItem.artTime ?: "0000-00-00 00:00:00")
         }
-    } else {
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.Start,
-        ) {
-            Text(
-                text = viewModel.artItem.artTitle ?: "",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(
-                    start = 5.dp, end = 5.dp,
-                    top = 10.dp, bottom = 10.dp
-                )
-            )
 
-            Row(
+        Row(modifier = Modifier.padding(start = 5.dp, bottom = 10.dp)) {
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = null,
+                tint = Color.Red,
+                modifier = Modifier.size(20.dp, 20.dp),
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(text = (viewModel.artItem.artLike ?: 0).toString())
+        }
+
+        Divider(modifier = Modifier.fillMaxWidth(), thickness = 2.dp)
+
+        Text(
+            text = viewModel.artItem.artContent ?: "",
+            modifier = Modifier.padding(
+                start = 5.dp, end = 5.dp,
+                top = 10.dp, bottom = 10.dp
+            ),
+            fontSize = 22.sp
+        )
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Icon(
+                imageVector =
+                if (!isLike)
+                    ImageVector.vectorResource(id = R.drawable.heart_plus)
+                else
+                    ImageVector.vectorResource(id = R.drawable.heart_check),
+                contentDescription = null,
+                tint = Color.Red,
                 modifier = Modifier
-                    .padding(end = 5.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.End
-            ) {
-                Icon(Icons.Default.Person, contentDescription = null)
-                Text(text = viewModel.artItem.artAuthorName ?: "")
-                Spacer(modifier = Modifier.width(10.dp))
-                Icon(Icons.Default.Schedule, null)
-                Text(text = viewModel.artItem.artTime ?: "0000-00-00 00:00:00")
-            }
-
-            Row(modifier = Modifier.padding(start = 5.dp, bottom = 10.dp)) {
-                Icon(
-                    imageVector = Icons.Default.Favorite,
-                    contentDescription = null,
-                    tint = Color.Red,
-                    modifier = Modifier.size(20.dp, 20.dp),
-                )
-                Spacer(modifier = Modifier.width(5.dp))
-                Text(text = (viewModel.artItem.artLike ?: 0).toString())
-            }
-
-            Divider(modifier = Modifier.fillMaxWidth(), thickness = 2.dp)
-
-            Text(
-                text = viewModel.artItem.artContent ?: "",
-                modifier = Modifier.padding(
-                    start = 5.dp, end = 5.dp,
-                    top = 10.dp, bottom = 10.dp
-                ),
-                fontSize = 22.sp
-            )
-
-            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                Icon(
-                    imageVector =
-                    if (!isLike)
-                        ImageVector.vectorResource(id = R.drawable.heart_plus)
-                    else
-                        ImageVector.vectorResource(id = R.drawable.heart_check),
-                    contentDescription = null,
-                    tint = Color.Red,
-                    modifier = Modifier
-                        .size(50.dp, 50.dp)
-                        .clickable {
-                            if (!isLike) {
-                                Log.i(">>>>>>>>>>", viewModel.artItem!!.artLike.toString())
-                                coroutineScope.launch { viewModel.like(user_id = 0) }
-                                Log.i(">>>>>>>>>>", viewModel.artItem!!.artLike.toString())
-                            } else {
-                                viewModel.artItem!!.artLike?.minus(1)
-                            }
-                            isLike = !isLike
+                    .size(50.dp, 50.dp)
+                    .clickable {
+                        if (!isLike) {
+//                                Log.i(">>>>>>>>>>", viewModel.artItem!!.artLike.toString())
+                            coroutineScope.launch { viewModel.like(user_id = 0) }
+//                                Log.i(">>>>>>>>>>", viewModel.artItem!!.artLike.toString())
+                        } else {
+                            coroutineScope.launch { viewModel.dislike(user_id = 0) }
                         }
-                )
-            }
-            Divider(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 10.dp), thickness = 2.dp
-            )
-
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(100.dp)
-                    .padding(
-                        start = 10.dp, end = 10.dp
-                    ),
-                textStyle = TextStyle(
-                    fontSize = 22.sp
-                )
+                        isLike = !isLike
+                    }
             )
         }
+        Divider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp), thickness = 2.dp
+        )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(
+                    start = 10.dp, end = 10.dp
+                ),
+            textStyle = TextStyle(
+                fontSize = 22.sp
+            ),
+            placeholder = {
+                Text(text = "添加评论")
+            },
+            label = {
+                Row {
+                    Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+                    Text(text = "评论", fontSize = 20.sp)
+                }
+            },
+        )
     }
+
 }
--- app/src/main/java/com/example/homework/HotPage.kt
+++ app/src/main/java/com/example/homework/HotPage.kt
@@ -1,20 +1,20 @@
 package com.example.homework
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.homework.component.*
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.example.homework.component.MyExtendScaffold
 import com.example.homework.compositionLocal.LocalNavController
 import com.example.homework.compositionLocal.LocalUserViewModel
 import com.example.homework.ui.theme.Purple500
@@ -22,13 +22,20 @@ import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.accompanist.insets.statusBarsHeight
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
+@ExperimentalCoilApi
 @Preview(showBackground = true)
 @Composable
 fun HotView() {
 
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val userViewModel = LocalUserViewModel.current
     val navHostController = LocalNavController.current
+
+    val list: List<String> = listOf(
+        "https://i2.hdslb.com/bfs/face/d399d6f5cf7943a996ae96999ba3e6ae2a2988de.jpg",
+        "https://i1.hdslb.com/bfs/face/8895c87082beba1355ea4bc7f91f2786ef49e354.jpg",
+        "https://i0.hdslb.com/bfs/face/566078c52b408571d8ae5e3bcdf57b2283024c27.jpg",
+        "https://i2.hdslb.com/bfs/face/31b0d4177a204e95e3489d5c420cea8d53c93fae.jpg"
+    )
 
     ProvideWindowInsets {
         rememberSystemUiController().setStatusBarColor(
@@ -45,16 +52,20 @@ fun HotView() {
                     .statusBarsHeight()
             )
             MyExtendScaffold {
-                LazyColumn(content = {
-                    val list = (0..100).toList().map {
-                        it.toString()
-                    }
+                LazyColumn(
+                    contentPadding = it
+                ) {
                     items(list) {
-                        list.forEach() {
-                            Text(text = it)
-                        }
+                        Image(
+                            painter = rememberImagePainter(
+                                data = it
+                            ),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.size(100.dp, 100.dp)
+                        )
                     }
-                })
+                }
             }
 
 
